30e48d473d89b57605ec4715aaf5d119102d2dd2
Bartus Norbert <bartusnorbert@Mac.fritz.box>
2024-10-06 22:04:03 +0200

    login page and homepage + checkout page + update

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import BookingProcess from "@/components/homepage/BookingProcess"
+import CTA from "@/components/homepage/CTA"
+import FeaturedMenu from "@/components/homepage/FeaturedMenu"
+import HeroSection from "@/components/homepage/HeroSection"
+import Testimonials from "@/components/homepage/Testimonials"
+
+
+const HomePage = () => {
+  return (
+
+    <>
+    <HeroSection/>
+    <FeaturedMenu/>
+    <BookingProcess/>
+    <Testimonials/>
+    <CTA/>
+    </>
+  )
+}
+
+export default HomePage
